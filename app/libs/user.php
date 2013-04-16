76da08ad18486b482cd59cb6c8f71c6ca1514fd2
Quentin <Quentin@Quentin-PC>
2013-04-16 11:46:39 +0200

    New User library => Comment + New fonctions + Change on variables

--- app/libs/user.php
+++ app/libs/user.php
@@ -1,31 +1,34 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* Class : libs_user
+ * Function : have all informations on each page of the user actually logged
+ * Version : 0.1
+ * Date : 16/04/13
+ * @Author : Miki and Quentin
  */
 
-/**
- * Description of user
- *
- * @author Miki
- */
 class libs_user {
 
+    // Informations of the user 
     private $userData;
 
-    function __construct($id) { //fill with data
+    // All informations of the member are stocked in the global variable $userData
+    function __construct($id) {
         global $reg;
+        // If there is no ID in parametre 
         if (!isset($id))
             $id = "-1";
-        $data = mysql_query("SELECT * from member WHERE id_member = '$id'", $reg->dbcon);
-        while ($row = mysql_fetch_assoc($data)) {
+
+        // Select basic member informations + member_details
+        $query = mysql_query("SELECT * FROM member M, member_details D WHERE M.id_member = D.id_member AND M.id_member = '$id'", $reg->dbcon);
+        while ($row = mysql_fetch_assoc($query)) {
             foreach ($row as $key => $value) {
                 $this->userData[$key] = $value;
             }
         }
     }
 
+    // Get the information you need in the $name variable
     function __get($name) {
         if (isset($this->userData[$name]))
             return $this->userData[$name];
@@ -33,16 +36,33 @@ class libs_user {
             return 0;
     }
 
+    // Change an information of the member 
     function __set($name, $value) {
         $this->userData[$name] = $value;
     }
 
+    // Check if the information is stocked or not
     function __isset($name) {
         return isset($this->userData[$name]);
     }
-    function loggedin(){
-        return isset($_SESSION['idmember']);
-        
+
+    // Check is an id is stocked on a session or not : if the member is logged or not
+    function loggedin() {
+        return isset($_SESSION['id_member']);
+    }
+
+    // Delete a selected informations 
+    function __delete($name) {
+        if (isset($this->userData[$name])) {
+            unset($this->userData[$name]);
+        }
+    }
+
+    // Update an information
+    function __update($name, $value) {
+        if ($this->userData[$name] != $value) {
+            $this->userData[$name] = $value;
+        }
     }
 }
 
