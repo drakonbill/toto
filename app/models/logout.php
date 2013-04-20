017a491d6d4833b0b19d71c03f769c60fd078d6e
Quentin <Quentin@Quentin-PC>
2013-04-20 15:13:08 +0200

    Logout model (New)

---
+++ app/models/logout.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Description of logout Model
+ * Logout of the website : destroy the id_member in Session.
+ * @author Quentin
+ */
+class models_logout extends Model {
+
+    public function indexModel() {
+        
+        // Unset id of the member in session
+        unset($_SESSION['id_member']);
+        
+        // Destroying the cookie
+        setcookie("email_member", "", time() - 3600);
+        setcookie("password_member", "", time() - 3600);
+        
+        // Index redirection
+        header('Location: /index');
+    }
+
+}
+
+?>
