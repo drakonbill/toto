6ea76f2ca149ed4530b8d0504ab92c65470d0195
Quentin <Quentin@Quentin-PC>
2013-04-22 23:54:12 +0200

    New controller for lostpassword.

---
+++ app/controllers/lostpassword.php
@@ -0,0 +1,22 @@
+<?php
+
+class controllers_lostpassword extends Controller {
+
+    function indexAction() {
+
+        $user = $this->reg->user;
+
+//load model and fill data with
+        $indexM = $this->loadModel("lostpassword");
+        $index = $indexM->indexModel();
+
+//load views
+        $this->loadView("header", "");
+        $this->loadView("lostpassword", $index);
+        $this->loadView("footer", "");
+    }
+
+}
+
+?>
+?>
