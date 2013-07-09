2c927f8f7e303bcea75f0be9d1237285f6fd7ac8
Quentin <Quentin@Quentin-PC>
2013-07-09 15:29:13 +0200

    New file with a lot of constants like in the profil part ... If you have a doubt on something, it's in this file.

---
+++ app/config/constants.php
@@ -0,0 +1,37 @@
+<?php
+
+/*--------------------- FOR THE PROFILE PART  -------------------- */
+ // Constants for the sex
+ 
+define("HOMME", "1");
+define("FEMME", "2");
+
+// When an information is missing
+define("NC", "Non communiqué");
+define("NCE", "Non communiquée");
+
+// Status of the member : Single... In couple ...
+define("SINGLE", "Célibataire");
+define("COUPLE", "En couple");
+define("BRIDE", "Mariée");
+define("MARIED", "Marié");
+
+// Preference : Man, Woman, or Both
+define("P_M", "1");
+define("P_W", "2");
+define("P_B", "3");
+
+// Professionnal situations
+define("COLLEGE", "1");
+define("LYCEE", "2");
+define("BAC1", "3");
+define("BAC2", "4");
+define("BAC3", "5");
+define("BAC4", "6");
+define("BAC5", "7");
+// > BAC + 5
+define("BAC6", "8");
+define("JOBLOOKING", "9");
+define("EMPLOYEE", "10");
+define("FREELANCE", "11");
+?>
