df24414f140e9cc448940413d05fbd5993b3a265
Quentin <Quentin@Quentin-PC>
2013-07-25 16:27:12 +0200

    You can modify 4 fileds on your profile.

--- public_html/js/profil/modify-profil.js
+++ public_html/js/profil/modify-profil.js
@@ -1,12 +1,9 @@
 $( document ).ready(function() {
-
-    // For the first name of the member
-    $('#first-name').editable(function(value, settings) { 
-       
-        /* console.log(this);
+     
+    function firstnameajax(value, settings){
+        console.log(this);
         console.log(value);
         console.log(settings);
-        return(value); */
         
         $.ajax({
             type: 'POST',
@@ -14,14 +11,109 @@ $( document ).ready(function() {
             data: "first-name="+value,
             success:
             function(result) {
-             //  alert(result); 
+            // alert(result); 
             }
         });
+    }
+    
+    function pseudoajax(value, settings){
+        console.log(this);
+        console.log(value);
+        console.log(settings);
+        
+        $.ajax({
+            type: 'POST',
+            url: '/ajax/modifyPseudo',
+            data: "pseudo="+value,
+            success:
+            function(result) {
+            //  alert(result); 
+            }
+        });
+    }
+    
+    function situationajax(value, settings){
+        console.log(this);
+        console.log(value);
+        console.log(settings);
+        
+        $.ajax({
+            type: 'POST',
+            url: '/ajax/modifySituation',
+            data: "situation="+value,
+            success:
+            function(result) {
+            //  alert(result); 
+            }
+        });
+    }
+    
+    function preferanceajax(value, settings){
+        console.log(this);
+        console.log(value);
+        console.log(settings);
+        
+        $.ajax({
+            type: 'POST',
+            url: '/ajax/modifyPreferance',
+            data: "preferance="+value,
+            success:
+            function(result) {
+            //  alert(result); 
+            }
+        });
+    }
+    
+    $('#first-name').editable(function(value, settings) { 
+        firstnameajax(value, settings);
+        return(value);
     }, { 
         type    : 'text',
         submit  : 'Modifier',
         cancel  : 'Annuler',
         width   : '200px',
     });
+ 
+    $('#pseudo').editable(function(value, settings) { 
+        pseudoajax(value, settings);
+        return(value);
+    }, { 
+        type    : 'text',
+        submit  : 'Modifier',
+        cancel  : 'Annuler',
+        width   : '200px',
+    }); 
+
+    $('#situation1').editable(function(value, settings) { 
+        situationajax(value, settings);
+        return(value);
+    }, { 
+        data    : "{'Non communiquée':'Non communiquée', 'Célibataire':'Célibataire','Couple':'Couple','Marié':'Marié'}",
+        type    : 'select',
+        submit  : 'Modifier',
+        cancel  : 'Annuler',
+        width   : '200px',
+    });
     
+    $('#situation2').editable(function(value, settings) { 
+        situationajax(value, settings);
+        return(value);
+    }, { 
+        data    : "{'Non communiquée':'Non communiquée', 'Célibataire':'Célibataire','Couple':'Couple','Mariée':'Mariée'}",
+        type    : 'select',
+        submit  : 'Modifier',
+        cancel  : 'Annuler',
+        width   : '200px',
+    });
+    
+    $('#preferance').editable(function(value, settings) { 
+        preferanceajax(value, settings);
+        return(value);
+    }, { 
+        data    : "{'Non communiqué':'Non communiqué', 'Homme':'Homme', 'Femme':'Femme', 'Homme et Femme':'Homme et Femme' }",
+        type    : 'select',
+        submit  : 'Modifier',
+        cancel  : 'Annuler',
+        width   : '200px',
+    });
 });
