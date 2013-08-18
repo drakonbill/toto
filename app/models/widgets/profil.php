5fb97e59a468343db6d020a9f2aca86a2f600f6e
Quentin <Quentin@Quentin-PC>
2013-08-18 17:31:03 +0200

    Installation of the passion widget

---
+++ app/models/widgets/profil.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Description of news
+ *
+ * @author Miki
+ */
+class models_widgets_profil extends Model {
+    
+     public function init() {
+         
+         $test = "test";
+        
+         return $test;
+    }
+}
+
+?>
