151a661ec64d2f334e4c4ce9cd4f7b0a7a064f74
Quentin <Quentin@Quentin-PC>
2013-08-21 18:02:50 +0200

    Widgets of passion working now with the id in param in the widgets.

--- app/models/widgets/profilPassion.php
+++ app/models/widgets/profilPassion.php
@@ -7,15 +7,13 @@
  */
 class models_widgets_profilPassion extends Model {
 
-    public function passionCategory() {
+    public function passionCategory($idmember) {
 
         $requete_passion = mysql_query("SELECT * FROM passion_category");
 
         while ($result = mysql_fetch_assoc($requete_passion)) {
             $data[$result['id_category']] = $result;
         }
-
-        $idmember = $_SESSION['id_member'];
 
         $requete_memberpassion = mysql_query("SELECT * FROM passion_category C, member_passion M, passion P WHERE M.id_member = ".$idmember." AND P.id_passion = M.id_passion AND P.id_category = C.id_category");
         
