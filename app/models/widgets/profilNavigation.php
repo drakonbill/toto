6685ac33a5ad12172b7ad00d7eb4b473b75dd213
Quentin <Quentin@Quentin-PC>
2013-08-28 17:17:41 +0200

    Repearing some broken code.

--- app/models/widgets/profilNavigation.php
+++ app/models/widgets/profilNavigation.php
@@ -7,19 +7,8 @@
  */
 class models_widgets_profilNavigation extends Model {
 
-    public function profilNavigation($datasent) {
+    public function profilNavigation($data) {
 
-        $id = $datasent["id_member"];
-        $own = $datasent["own"];
-
-        $requete_member = mysql_query("SELECT * FROM member WHERE id_member = " . $id . "");
-
-        while ($donnees = mysql_fetch_assoc($requete_member)) {
-            $data["id_member"] = $donnees["id_member"];
-            $data["own"] = $own;
-            $data["pseudo_member"] = $donnees["pseudo_member"];
-            $data["photo_member"] = $donnees["photo_member"];
-        }
         return $data;
     }
 
